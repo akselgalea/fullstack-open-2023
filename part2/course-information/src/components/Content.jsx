c2afb6cf87002b40dfd0368780ab99d30830b705
Aksel Galea <akselgalea@gmail.com>
2023-09-11 10:50:21 -0300

    Part 2 - Exercises 2.2 and 2.3

--- part2/course-information/src/components/Content.jsx
+++ part2/course-information/src/components/Content.jsx
@@ -1,11 +1,16 @@
 import Part from './Part'
+import Total from './Total'
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((sum, current) => sum + current.exercises, 0)
+
   return (
     <>
       {parts.map((part) => 
         <Part key={part.id} part={part} />
       )}
+
+      <Total total={total} />
     </>
   )
 }
