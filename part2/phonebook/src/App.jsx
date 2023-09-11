003e0ed7a6d8688f126ff429422d5a8e98d37c0f
Aksel Galea <akselgalea@gmail.com>
2023-09-11 18:33:20 -0300

    Part 2 - Exercise 2.11 + text for filter

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      setPersons(res.data)
+    })
+  }, [])
 
   const filteredPersons = !filter ? persons : persons.filter(person => person.name.toLowerCase().includes(filter))
 
@@ -59,7 +60,9 @@ const App = () => {
           <h2>Phonebook</h2>
         </header>
 
-        <Filter value={filter} handleChange={handleFilterChange} />
+        <div>
+          filter shown with <Filter value={filter} handleChange={handleFilterChange} />
+        </div>
 
         <PersonForm
           name={newName}
