e4e76f2d5f1e0e1a118482818d3e2e8cc34bfffa
Aksel Galea <akselgalea@gmail.com>
2023-09-10 21:08:42 -0300

    Part 1 - Exercise 1.11 + added the % to the positive statistic

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 } 
 
@@ -14,14 +17,16 @@ const Statistics = ({ good, neutral, bad }) => {
   if(all === 0) return <p>No feedback given</p>
 
   return (
-    <>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={`${positive} %`} />
+      </tbody>
+    </table>
   )
 }
 
