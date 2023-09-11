1af1b5b3f031e5f424eec8c37c5c84b225bb5a39
Aksel Galea <akselgalea@gmail.com>
2023-09-11 11:34:41 -0300

    Part 2 - Exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,20 +6,45 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
+    <>
+      <div>
+        <header>
+          <h2>Phonebook</h2>
+        </header>
+
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input name="name" value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <header>
+          <h2>Numbers</h2>
+        </header>
+        
         <div>
-          name: <input />
+          {persons.map(person => 
+            <p key={person.name}>{person.name}</p>
+          )}
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
+      </div>
+    </>
   )
 }
 
